4f810bb2eb29ee954df1e8d1b85087bb0833fdd1
Jonas Lima de Amorim <dev.amorim@outlook.com>
2021-01-17 19:59:01 -0300

    fix: returing absolute urls in api

--- pages/api/team.js
+++ pages/api/team.js
@@ -1,11 +1,11 @@
 export default function handler(req, res) {
   var team = [
-    { id: 1, name: 'Bill Mahoney', role: 'Product Owner', picture: '/img/photo1.png' },
-    { id: 2, name: 'Saba Cabrera', role: 'Art Director', picture: '/img/photo2.png' },
-    { id: 3, name: 'Shae Le', role: 'Tech Lead', picture: '/img/photo3.png' },
-    { id: 4, name: 'Skylah Lu', role: 'UX Designer', picture: '/img/photo4.png' },
-    { id: 5, name: 'Griff Richards', role: 'Developer', picture: '/img/photo5.png' },
-    { id: 6, name: 'Stan John', role: 'Developer', picture: '/img/photo6.png' }
+    { id: 1, name: 'Bill Mahoney', role: 'Product Owner', picture: `${process.env.BASE_URL}/img/photo1.png` },
+    { id: 2, name: 'Saba Cabrera', role: 'Art Director', picture: `${process.env.BASE_URL}/img/photo2.png` },
+    { id: 3, name: 'Shae Le', role: 'Tech Lead', picture: `${process.env.BASE_URL}/img/photo3.png` },
+    { id: 4, name: 'Skylah Lu', role: 'UX Designer', picture: `${process.env.BASE_URL}/img/photo4.png` },
+    { id: 5, name: 'Griff Richards', role: 'Developer', picture: `${process.env.BASE_URL}/img/photo5.png` },
+    { id: 6, name: 'Stan John', role: 'Developer', picture: `${process.env.BASE_URL}/img/photo6.png` }
   ];
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
